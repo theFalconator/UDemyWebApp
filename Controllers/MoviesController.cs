859f676866f5be7a2703da9c363e4c707468f194
theFalconator <falcon.nick@gmail.com>
2017-12-08 11:36:16 -0600

    solution to excersize 2

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using UDemyWebApplication.Models;
 using UDemyWebApplication.ViewModels;
@@ -11,7 +12,7 @@ namespace UDemyWebApplication.Controllers
         // GET
         public ActionResult Random()
         {
-            var movie = new Movie(1, "Shrek");
+            var movie = new Movie {Id=1, Name="Shrek"};
             var customers = new List<Customer>
             {
                 new Customer() {Name = "Customer 1"},
@@ -34,21 +35,34 @@ namespace UDemyWebApplication.Controllers
             return Content("id=" + id);
         }
 
-        public ActionResult Index(int? pageIndex, string sortBy)
+        public ViewResult Index()
         {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-            
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-            
-            return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
+            var movies = GetMovies();
+            return View(movies);
         }
+        
+        
         
         [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year + "/" + month);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name = "Shrek"},
+                new Movie {Id = 2, Name = "Wall-e"}
+            };
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = GetMovies().SingleOrDefault(m => m.Id == id);
+            return View(movie);
+            
         }
     }
 }
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -5,10 +5,5 @@
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public Movie(int id, string name)
-        {
-            Id = id;
-            Name = name;
-        }
     }
 }
